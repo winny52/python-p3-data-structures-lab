a2cd02d2b4515a3e4818d85e08e53930c66f5c23
Winny <winny.chelangat2@student.moringaschool.com>
2023-08-17 20:17:41 +0300

    Completed lab

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -1,38 +1,40 @@
-spicy_foods = [
-    {
-        "name": "Green Curry",
-        "cuisine": "Thai",
-        "heat_level": 9,
-    },
-    {
-        "name": "Buffalo Wings",
-        "cuisine": "American",
-        "heat_level": 3,
-    },
-    {
-        "name": "Mapo Tofu",
-        "cuisine": "Sichuan",
-        "heat_level": 6,
-    },
-]
-
 def get_names(spicy_foods):
-    pass
+    return [food["name"] for food in spicy_foods]
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [food for food in spicy_foods if food["heat_level"] > 5]
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        heat_level = "🌶" * food["heat_level"]
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level}")
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    spiciest_foods = get_spiciest_foods(spicy_foods)
+    print_spicy_foods(spiciest_foods)
 
-def get_average_heat_level(spicy_foods):
-    pass
+def average_heat_level(spicy_foods):
+    total_heat = sum(food["heat_level"] for food in spicy_foods)
+    num_foods = len(spicy_foods)
+    return total_heat // num_foods
 
-def create_spicy_food(spicy_foods, spicy_food):
-    pass
+# Example spicy foods list
+spicy_foods = [
+    {"name": "Green Curry", "cuisine": "Thai", "heat_level": 9},
+    {"name": "Buffalo Wings", "cuisine": "American", "heat_level": 3},
+    {"name": "Mapo Tofu", "cuisine": "Sichuan", "heat_level": 6}
+]
+
+# Test the functions
+if __name__ == "__main__":
+    print(get_names(spicy_foods))
+    print(get_spiciest_foods(spicy_foods))
+    print_spicy_foods(spicy_foods)
+    print(get_spicy_food_by_cuisine(spicy_foods, "American"))
+    print_spiciest_foods(spicy_foods)
+    print(average_heat_level(spicy_foods))
